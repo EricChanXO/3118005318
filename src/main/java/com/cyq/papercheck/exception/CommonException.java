6a9bcea2dfb7ea94d134beb49b3dc8dc4403a118
EricChanXO <1459405650@qq.com>
2020-09-22 20:15:05 +0800

    feat(exception):新增全局自定义异常处理

---
+++ src/main/java/com/cyq/papercheck/exception/CommonException.java
@@ -0,0 +1,11 @@
+package com.cyq.papercheck.exception;
+
+/**
+ * @author cyq
+ * @date 2020/9/22
+ */
+public class CommonException extends RuntimeException{
+    public CommonException(String message) {
+        super(message);
+    }
+}
